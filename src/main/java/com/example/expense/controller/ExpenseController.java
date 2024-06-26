0c5632112a592b2ad5d243d76ecf26d083115465
Ahana416 <125171139+Ahana416@users.noreply.github.com>
2024-06-26 06:16:08 +0530

    Update ExpenseController.java

--- src/main/java/com/example/expense/controller/ExpenseController.java
+++ src/main/java/com/example/expense/controller/ExpenseController.java
@@ -13,65 +13,30 @@ import java.util.Optional;
 public class ExpenseController {
 
     @Autowired
-    private ItemService itemService;
+    private ExpenseService expenseService;
 
     @GetMapping
     public List<Item> getAllItems() {
-        return itemService.getAllItems();
+        return expenseService.getAllItems();
     }
 
     @GetMapping("/{id}")
     public Optional<Item> getItemById(@PathVariable Long id) {
-        return itemService.getItemById(id);
+        return expenseService.getItemById(id);
     }
 
     @GetMapping("/category/{category}")
     public List<Item> getItemsByCategory(@PathVariable String category) {
-        return itemService.getItemsByCategory(category);
+        return expenseService.getItemsByCategory(category);
     }
 
     @PostMapping
     public Item createItem(@RequestBody Item item) {
-        return itemService.saveItem(item);
+        return expenseService.saveItem(item);
     }
 
     @DeleteMapping("/{id}")
     public void deleteItem(@PathVariable Long id) {
-        itemService.deleteItem(id);
+        expenseService.deleteItem(id);
     }
 }
-
-// package com.example.expense.controller;
-
-// import com.example.expense.model.Item;
-// import com.example.expense.service.ExpenseService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-
-// @RestController
-// @RequestMapping("/Expense")
-// public class ExpenseController {
-//     private final ExpenseService expenseService;
-
-//     @Autowired
-//     public ExpenseController(ExpenseService expenseService) {
-//         this.expenseService = expenseService;
-//     }
-
-//     @PostMapping("/add")
-//     public void addExpense(@RequestBody Item item) {
-
-//     }
-
-//     @DeleteMapping("/remove")
-//     public void removeItem(@RequestBody Item item) {
-//     }
-
-//     @PostMapping("/borrow")
-//     public void borrowItem(@RequestBody Item item) {
-//     }
-
-//     @PostMapping("/return")
-//     public void returnItem(@RequestBody Item item) {
-//     }
-// }
